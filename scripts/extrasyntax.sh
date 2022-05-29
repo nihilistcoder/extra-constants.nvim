d177d616e3fb181a50b5f71d7edb8f2504b8073e
gmbo <gg.mbrb@gmail.com>
2022-05-29 17:46:53 -0300

    plugin now correctly removes any constant that appears in nvim's c.vim

--- scripts/extrasyntax.sh
+++ scripts/extrasyntax.sh
@@ -8,16 +8,16 @@ CPP=$(which cpp)
 DIFF=$(which diff)
 
 function find_constants () {
-    [[ -z "${1}" || ! -f "${1}" ]] && echo "" && return 1
-    [[ ! -f "${2}/predefs.txt" ]] && echo "" && return 1
-
     FILE=${1}
     CACHE_DIR=${2}
+
+    [[ -z "${FILE}" || ! -f "${FILE}" || ! -f "${CACHE_DIR}/predefs.txt" ]] && return 1
+
     TMPFILE=$(mktemp)
 
     ${CPP} -dD -E -x c ${FILE} 2>/dev/null | ${GREP} "#define" | cut -d' ' -f2 | grep -v -E -e "\w+\(.*\)$" > ${TMPFILE}
 
-    grep ${TMPFILE} -v -f "${CACHE_DIR}/predefs.txt" | grep -v -E -e "(^_.*|_H(_INCLUDED)?$)"
+    grep ${TMPFILE} -v -f "${CACHE_DIR}/predefs.txt" | grep -v -f "${CACHE_DIR}/nvim_syntax.txt" | grep -v -E -e "(^_.*|_H(_INCLUDED)?$)"
 
     rm ${TMPFILE}
 
---
+++ scripts/regenerate_nvim_syntax.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+NVIM_SYNTAX_FILE="/usr/share/nvim/runtime/syntax/c.vim"
+
+CACHE_DIR=${1}
+
+[[ -z "${CACHE_DIR}" || ! -d "${CACHE_DIR}" ]] && echo "usage: ./regenerate_nvim_syntax.sh <CACHE_DIR>" && exit 1
+
+NVIM_SYNTAX_TXT="${CACHE_DIR}/nvim_syntax.txt"
+
+grep ${NVIM_SYNTAX_FILE} -E -e "syn\s*keyword\s*cConstant" | sed "s/^\s*syn\s*keyword\s*cConstant\s*//" | tr " " "\n" > ${NVIM_SYNTAX_TXT}
+grep ${NVIM_SYNTAX_FILE} -E -e "syn\s*keyword\s*cType" | sed "s/^\s*syn\s*keyword\s*cType\s*//" | tr " " "\n" >> ${NVIM_SYNTAX_TXT}
