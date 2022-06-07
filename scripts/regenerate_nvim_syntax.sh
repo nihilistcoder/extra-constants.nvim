c06b816b6f3c433b7d821c94600737d4e04ffa48
gmbo <gg.mbrb@gmail.com>
2022-06-06 21:18:10 -0300

    update scripts/regenerate_nvim_syntax.sh to grep for cStorateClass

--- scripts/regenerate_nvim_syntax.sh
+++ scripts/regenerate_nvim_syntax.sh
@@ -10,3 +10,4 @@ NVIM_SYNTAX_TXT="${CACHE_DIR}/nvim_syntax.txt"
 
 grep ${NVIM_SYNTAX_FILE} -E -e "syn\s*keyword\s*cConstant" | sed "s/^\s*syn\s*keyword\s*cConstant\s*//" | tr " " "\n" > ${NVIM_SYNTAX_TXT}
 grep ${NVIM_SYNTAX_FILE} -E -e "syn\s*keyword\s*cType" | sed "s/^\s*syn\s*keyword\s*cType\s*//" | tr " " "\n" >> ${NVIM_SYNTAX_TXT}
+grep ${NVIM_SYNTAX_FILE} -E -e "syn\s*keyword\s*cStorageClass" | sed "s/^\s*syn\s*keyword\s*cStorageClass\s*//" | tr " " "\n" >> ${NVIM_SYNTAX_TXT}
